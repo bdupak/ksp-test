c0d9c2b2355b009fc1813708d0711fe0eb1eace5
Bohdan_Dupak <bohdan_dupak@epam.com>
2023-09-22 13:14:23 +0300

    added logic initialization array

--- src/main/java/com/lp/ittp/ist/App.java
+++ src/main/java/com/lp/ittp/ist/App.java
@@ -1,13 +1,13 @@
 package com.lp.ittp.ist;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  public static void main(String[] args) {
+    int[] array = new int[10];
+    int counter = 0;
+    for (int i = 0; i < array.length - 1; i++) {
+      array[i] = i;
+      counter++;
     }
+    System.out.println("Hello World!");
+  }
 }
