5ed04aa4288faa001404023bb74503421717b8d7
Bohdan Dupak <Dupak.Bohdan@externals.adidas.com>
2025-10-02 17:01:00 +0300

    added System.out.println for main method

--- src/main/java/com/lp/ittp/ist/App.java
+++ src/main/java/com/lp/ittp/ist/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
+    System.out.println("In this method we have done small logic");
     int[] array = new int[10];
     int counter = 0;
     for (int i = 0; i < array.length - 1; i++) {
